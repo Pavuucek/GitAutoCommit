ac1db3cb995e03202b95732152388685416edfd3
Michal Kuncl <michal.kuncl@gmail.com>
2015-09-28 16:58:39 +0200

    Cleanup code and add license: Support dir

--- src/GitAutoCommit/Support/DebugWindowWriter.cs
+++ src/GitAutoCommit/Support/DebugWindowWriter.cs
@@ -1,7 +1,37 @@
-﻿using System;
-using System.Collections.Generic;
+﻿#region License
+
+/*
+Copyright (c) 2011 Gareth Lennox (garethl@dwakn.com)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+    * Neither the name of Gareth Lennox nor the names of its
+    contributors may be used to endorse or promote products derived from this
+    software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+*/
+
+#endregion
+
+using System;
 using System.IO;
-using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
@@ -9,11 +39,16 @@ namespace GitAutoCommit.Support
 {
     public class DebugWindowWriter : TextWriter
     {
-        private TextBox textbox;
+        private readonly TextBox textbox;
 
         public DebugWindowWriter(TextBox textbox)
         {
             this.textbox = textbox;
+        }
+
+        public override Encoding Encoding
+        {
+            get { return Encoding.ASCII; }
         }
 
         public override void Write(string value)
@@ -38,11 +73,6 @@ namespace GitAutoCommit.Support
             {
                 textbox.AppendText(value + Environment.NewLine);
             }
-        }
-
-        public override Encoding Encoding
-        {
-            get { return Encoding.ASCII; }
         }
     }
 }
--- src/GitAutoCommit/Support/Interval.cs
+++ src/GitAutoCommit/Support/Interval.cs
@@ -1,4 +1,36 @@
-﻿namespace GitAutoCommit.Support
+﻿#region License
+
+/*
+Copyright (c) 2011 Gareth Lennox (garethl@dwakn.com)
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+    * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+    * Neither the name of Gareth Lennox nor the names of its
+    contributors may be used to endorse or promote products derived from this
+    software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. 
+*/
+
+#endregion
+
+namespace GitAutoCommit.Support
 {
     public class Interval
     {
@@ -15,15 +47,15 @@
                 return string.Format("{0} hours", Seconds/60/60);
 
             if (Seconds == 60*60)
-                return string.Format("1 hour");
+                return "1 hour";
 
             if (Seconds == 60)
-                return string.Format("1 minute");
+                return "1 minute";
 
             if (Seconds > 60)
                 return string.Format("{0} minutes", Seconds/60);
             if (Seconds == 1)
-                return string.Format("1 second");
+                return "1 second";
             return string.Format("{0} seconds", Seconds);
         }
     }
