88bffc43218a70a635964a4d059ba67f79a219f7
Michal Kuncl <michal.kuncl@gmail.com>
2015-09-28 22:12:50 +0200

    build debug and release version in build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,7 @@
 @ECHO OFF
 
 rem call version.bat
+%windir%\Microsoft.Net\Framework\v4.0.30319\msbuild src\GitAutoCommit.sln  /property:Configuration=Debug /property:Platform="Any CPU"
 %windir%\Microsoft.Net\Framework\v4.0.30319\msbuild src\GitAutoCommit.sln  /property:Configuration=Release /property:Platform="Any CPU"
 
 if ERRORLEVEL 1 pause
