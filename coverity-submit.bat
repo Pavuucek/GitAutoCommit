f72de022f0c3ef113d6cfcef824f3d851a3e4bb5
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-17 16:53:31 +0100

    coverity.bat coverity-submit.bat - better zip than tar... submission of both isn't working now for some reason.

--- coverity-submit.bat
+++ coverity-submit.bat
@@ -1,2 +1,2 @@
-@curl --form token=3aO3AS5M0k8tsNAXXMeKiQ --form email=michal.kuncl@gmail.com --form file=@coverity-GitAutoCommit.tar --form version="$MajorVersion$.$MinorVersion$.$Revision$-$Commit$-$ShortHash$" --form description="$Branch$:$MajorVersion$.$MinorVersion$.$Revision$-$Commit$-$ShortHash$" https://scan.coverity.com/builds?project=Pavuucek%2FGitAutoCommit
+@curl --form token=3aO3AS5M0k8tsNAXXMeKiQ --form email=michal.kuncl@gmail.com --form file=@coverity-GitAutoCommit.zip --form version="$MajorVersion$.$MinorVersion$.$Revision$-$Commit$-$ShortHash$" --form description="$Branch$:$MajorVersion$.$MinorVersion$.$Revision$-$Commit$-$ShortHash$" https://scan.coverity.com/builds?project=Pavuucek%2FGitAutoCommit
 @exit
--- coverity.bat
+++ coverity.bat
@@ -1,7 +1,7 @@
 @echo off
 if exist "cov-int" rd /q /s "cov-int"
 cov-build.exe --dir cov-int "build.bat"
-7z a -ttar "coverity-GitAutoCommit.tar" "cov-int"
+7z a -tzip "coverity-GitAutoCommit.zip" "cov-int"
 gitversioner w coverity-submit.bat
 call coverity-submit.bat
 gitversioner r coverity-submit.bat
