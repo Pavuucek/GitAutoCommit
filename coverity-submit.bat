450d3b5d98fd59f078c8a911a2d19fc39604e027
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-17 16:41:45 +0100

    fix coverity-submit.bat

--- coverity-submit.bat
+++ coverity-submit.bat
@@ -1,2 +1,2 @@
-@curl --form token=3aO3AS5M0k8tsNAXXMeKiQ --form email=michal.kuncl@gmail.com --form --form version="$Branch$:$MajorVersion$.$MinorVersion$.$Revision$-$Commit$-$ShortHash$" file=@coverity-GitAutoCommit.tar https://scan.coverity.com/builds?project=Pavuucek%2FGitAutoCommit
+@curl --form token=3aO3AS5M0k8tsNAXXMeKiQ --form email=michal.kuncl@gmail.com --form file=@coverity-GitAutoCommit.tar --form version="$Branch$:$MajorVersion$.$MinorVersion$.$Revision$-$Commit$-$ShortHash$" --form description="$MajorVersion$.$MinorVersion$.$Revision$-$Commit$-$ShortHash$" https://scan.coverity.com/builds?project=Pavuucek%2FGitAutoCommit
 @exit
