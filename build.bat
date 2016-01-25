4a71f26272312d70af47ac613e92b1d32a52171d
Michal Kuncl <michal.kuncl@gmail.com>
2016-01-25 16:23:46 +0100

    update GitVersioner.exe

--- build.bat
+++ build.bat
@@ -3,9 +3,8 @@
 rem call version.bat
 %windir%\Microsoft.Net\Framework\v4.0.30319\msbuild src\GitAutoCommit.sln /m /property:Configuration=Debug /property:Platform="Any CPU"
 %windir%\Microsoft.Net\Framework\v4.0.30319\msbuild src\GitAutoCommit.sln /m /property:Configuration=Release /property:Platform="Any CPU"
-
 if ERRORLEVEL 1 pause
-
+gitversioner.exe ba
 gitversioner.exe w zip.bat
 call zip.bat
 gitversioner r zip.bat
