08904fbeff309ad1475cb07a42ddb67686c5df5a
Michal Kuncl <michal.kuncl@gmail.com>
2016-11-30 16:06:37 +0100

    correct build.bat error

--- build.bat
+++ build.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
+pushd src
 nuget restore
+popd
 rem call version.bat
 %windir%\Microsoft.Net\Framework\v4.0.30319\msbuild src\GitAutoCommit.sln /m /property:Configuration=Debug /property:Platform="Any CPU"
 %windir%\Microsoft.Net\Framework\v4.0.30319\msbuild src\GitAutoCommit.sln /m /property:Configuration=Release /property:Platform="Any CPU"
